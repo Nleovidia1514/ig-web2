312e1d5e735699d6d831c46360fedb0cc37fec33
Leonardo Baron <lbaron@conocimientocorporativo.com>
2020-08-02 17:03:01 -0400

    cambiado puerto

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server listening on http://localhost:3000");
 });
 
